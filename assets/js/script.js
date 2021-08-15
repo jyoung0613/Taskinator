ada55330f3cb412503b469df8824926d1287473f
John Young <jwyoung0613@gmail.com>
2021-08-14 19:44:28 -0600

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-    console.log(event);
     event.preventDefault();
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-};
+    };
+
+    formEl.addEventListener("submit", createTaskHandler);
 
 formEl.addEventListener("submit", createTaskHandler)
 
